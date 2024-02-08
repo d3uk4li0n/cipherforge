a6d873e15745dceb3ae6b1a6440eb9dd376977f0
Lorenzo Cunial <99892625+d3uk4li0n@users.noreply.github.com>
2024-02-08 11:48:26 +0100

    Update cipherforge.py

--- cipherforge.py
+++ cipherforge.py
@@ -13,7 +13,14 @@ def permute_all(word):
 
 def rev_word(word):
     return word[::-1]
-
+    
+## add digits from 1 to 1000000
+def add_digits(word):
+    ret_list = []
+    for i in range(1, 10000001):
+        ret_list.append(word + str(i))
+    return ret_list
+    
 if __name__ == "__main__":
     file = input(f"{Fore.BLUE}[?] (Optional) output (default: cf_wordlist.txt): ")
     source = input(f"{Fore.BLUE}[?] Insert word list (separated by comma): ").split(", ")
@@ -22,3 +29,4 @@ if __name__ == "__main__":
       print(upper(s))
       print(permute_all(s))
       print(rev_word(s)) 
+      print(add_digits(s))
